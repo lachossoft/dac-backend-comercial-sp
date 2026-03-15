b53938137d4a2ce35ab866f011122824ed2db619
Horacio <151089759+devmfcancun@users.noreply.github.com>
2026-03-15 01:04:44 -0500

    Add: configuracion de variables de entorno

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Microservicio que permite la gestion de usuarios y el control de la autenticacio
 
 
 > npm install
+
+### Enviroment
+
+> PORT=numero de puerto
+
+### Ejecucion del proyecto para developers
+
+> npm run start:dev
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = new Logger('auth-ms');
+  await app.listen(envs.port, () => {
+    logger.log(`Auth microservice is running on port ${envs.port}`);
+  });
 }
+
 bootstrap();
